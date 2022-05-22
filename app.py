2d196a3cea3047b54c42c57e65c317b99f6faae7
ogog1 <gorkeyomer@gmail.com>
2022-05-22 22:28:38 +0300

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_entry, view_entries
+from database import add_entry, get_entries
 
 menu = """Please select one of the following options:
 1) Add new entry for today.
@@ -9,13 +9,25 @@ Your selection: """
 
 welcome = 'Welcome to your programming diary!'
 
+def prompt_new_entry():
+    entry_content = input('What have you learned today? ')
+    entry_date = input('Enter the date: ')
+
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
 print(welcome)
 
 while (user_input := input(menu)) != '3':
     if user_input == '1':
-        add_entry()
+        promp_new_entry()
     elif user_input == '2':
-        view_entries()
+        view_entries(get_entries())
     else:
         print('Invalid input, try again!')
+
+
 
