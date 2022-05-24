13bd564692c32400acc81458721a6576bfb1e23e
ogog1 <gorkeyomer@gmail.com>
2022-05-24 21:52:24 +0300

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_entry, get_entries
+from database import create_table, add_entry, get_entries
 
 menu = """Please select one of the following options:
 1) Add new entry for today.
@@ -20,10 +20,11 @@ def view_entries(entries):
         print(f"{entry['date']}\n{entry['content']}\n\n")
 
 print(welcome)
+create_table()
 
 while (user_input := input(menu)) != '3':
     if user_input == '1':
-        promp_new_entry()
+        prompt_new_entry()
     elif user_input == '2':
         view_entries(get_entries())
     else:
--- database.py
+++ database.py
@@ -1,8 +1,18 @@
-entries = []
+import sqlite3
+
+connection = sqlite3.connect('journal.db')
+
+def create_table():
+    with connection:
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);"
+        )
 
 def add_entry(entry_content, entry_date):
-    entries.append({'content': entry_content, 'date': entry_date})
-
+    with connection:
+        connection.execute(
+            "INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date)
+        )
 
 def get_entries():
     return entries
